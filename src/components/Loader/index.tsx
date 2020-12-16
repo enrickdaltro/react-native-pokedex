9f3726c9860aec192596940bfd87c2b14d973ec2
Enrick <enrickdaltro@gmail.com>
2020-12-16 12:26:09 -0300

    fix: Loader color

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
+import colors from '../../assets/colors';
 
 interface ILoaderProps {
   style?: { [key: string]: any };
@@ -9,7 +10,9 @@ interface ILoaderProps {
 const Loader: React.FC<ILoaderProps> = ({ style, loading }) => {
   return (
     <View style={[styles.container, style && style]}>
-      {loading && <ActivityIndicator size="large" />}
+      {loading && (
+        <ActivityIndicator size="large" color={colors.Black80} style={{ marginBottom: 40 }} />
+      )}
     </View>
   );
 };
